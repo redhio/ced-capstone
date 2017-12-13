44c14310db37cfedc0d57bbc66735673786910c4
redhio-don <34153381+redhio-don@users.noreply.github.com>
2017-12-13 18:52:45 -0500

    Add files via upload

---
+++ contracts/ServiceContract.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.8;
+
+contract ServiceContract {
+    //  This is a generic contract for a service
+    address public owner;
+    uint public xparam;
+    enum State { Created, Locked, Inactive } // Enum
+    enum ModelType { Iris, Iris2, SomethingElse } // Enum
+    
+    address public buyer;
+    address public seller;
+    address   public arbiter;
+    int public model;
+  
+    function ServiceContract(address _seller, address _arbiter, int _model) public {
+        buyer = msg.sender;
+        seller = _seller;
+        arbiter = _arbiter;
+        model = _model;
+    }
+
+
+
+ }

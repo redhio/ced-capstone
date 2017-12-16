6c06e4ff49a474b52019526c316451d7f705c640
Redhio Don <don@redh.io>
2017-12-15 22:11:17 -0500

    Friday V0.0.3

--- contracts/RemoteServicePurchase.sol
+++ contracts/RemoteServicePurchase.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.11;
-/* ced-capstone - This contract governs the transaction for a remote service.  
-* 
+/* ced-capstone - This contract governs the transaction for a remote machine learning service.  
+* The three entities which interact with the contract are the buyer, seller, and the external remote services.
 *
-*
+* Friday V0.0.3
 */
 contract Purchase {
     uint public value;
@@ -13,8 +13,13 @@ contract Purchase {
     
     enum State { Created, Locked, Inactive }
     State public state;
+    /* These should be in an abstracted class specific to the machine learning model */
+    uint petal_length;
+    uint petal_width;
+    uint sepal_length;
+    
 
-    // Ensure that `msg.value` is equal to our selling price
+    /* Set the purchase price requirement of the remote machine learning service. */
     function Purchase() public payable {
         seller = msg.sender;
         require(  2  == msg.value);
@@ -50,38 +55,25 @@ contract Purchase {
 
     /* Abort the purchase and reclaim the ether by the buyer or seller before
     *  the contract is locked for servicing. */
-    function abort() public
-        onlySeller
-        inState(State.Created)
-    {
-        Aborted();
+    function abort() public onlySeller inState(State.Created) {
+        Aborted(); /* FIRE the Aborted Event */
         state = State.Inactive;
         seller.transfer(this.balance);
     }
 
-    /// Confirm the purchase as buyer.
-    /// Transaction has to include `2 * value` ether.
-    /// The ether will be locked until confirmReceived
-    /// is called.
-    function confirmPurchase() public
-        inState(State.Created)
-        isEqual(msg.value == (2 * value))
-        payable
-    {
-        PurchaseConfirmed();
+    /* Confirm the purchase and the price to further lock the ether until the service is received. confirmReceived()*/
+    function confirmPurchase() public inState(State.Created) isEqual(msg.value == (2 * value)) payable {
+        PurchaseConfirmed(); /* FIRE the PurchaseConfirmed Event */
         buyer = msg.sender;
         state = State.Locked;
     }
 
     /* Confirm that the buyer has received the prediction, sla and can unlocked the Ether. */
-    function confirmReceived() public
-        onlyBuyer
-        inState(State.Locked)
+    function confirmReceived() public onlyBuyer inState(State.Locked)
     {
         if (serviceAccuracy == 1){
-        ServiceReceived();
-        /* Change the state to inactive to complete the final state. */
-        state = State.Inactive;
+        ServiceReceived(); /* FIRE the ServiceReceived Event */
+        state = State.Inactive; /* Change the state to inactive to complete the final state. */
 
         /* NOTE: This allows both the buyer and the seller to block the refund - the withdraw pattern should be used. */
 

2ea8d4eef00d0dffffcea2df3fecc2359bcb214b
Redhio Don <don@redh.io>
2017-12-16 18:01:02 -0500

    Saturday V0.2.0

--- contracts/RemoteServicePurchase.sol
+++ contracts/RemoteServicePurchase.sol
@@ -9,21 +9,29 @@ contract Purchase {
     address public seller;
     address public buyer;
     address public service;
-    int public serviceAccuracy;
+    int public sla;
+    int public slAccuracy;
     
     enum State { Created, Locked, Inactive }
     State public state;
-    /* These should be in an abstracted class specific to the machine learning model */
-    uint public petal_length;
-    uint public petal_width;
-    uint public sepal_length;
+    /*
+    * MLaaS - These should be in an abstracted class specific to the machine learning model 
+    */
+    uint public modelID;
+    uint public petalLength;
+    uint public petalWidth;
+    uint public sepalLength;
     string public prediction;
     
 
     /* Set the purchase price requirement of the remote machine learning service. */
-    function Purchase() public payable {
+    function Purchase(uint _modelID, uint _petalLength, uint _petalWidth, uint _sepalLength) public payable {
         seller = msg.sender;
         require(  2  == msg.value);
+        modelID = _modelID;
+        petalLength = _petalLength;
+        petalWidth = _petalWidth;
+        sepalLength = _sepalLength;        
     }
     /* boolean logical function */
     modifier isEqual(bool _condition) {
@@ -45,11 +53,22 @@ contract Purchase {
         require(msg.sender == service);
         _;
     }
+    /* Check to see the 'Service Level Acheived is greater or equal to Service Level Agreed' for the machine learning RemoteService */
+    modifier isSLA() {
+        require(slAccuracy >= sla);
+        _;
+    }
+    /* Check to see the seller is not a liar */
+    modifier isLiar() {
+        require(slAccuracy < sla);
+        _;
+    }
     /* Restrict the function to only a certain State.  Useful to step through a sequence of events*/
     modifier inState(State _state) {
         require(state == _state);
         _;
     }
+    /* Define all of the contract events here.  One for each state  */
     event Aborted();
     event PurchaseConfirmed();
     event ServiceReceived();
@@ -70,22 +89,19 @@ contract Purchase {
     }
 
     /* Confirm that the buyer has received the prediction, sla and can unlocked the Ether. */
-    function confirmReceived() public onlyBuyer inState(State.Locked)
+    function confirmReceived() public onlyBuyer isSLA inState(State.Locked)
     {
-        if (serviceAccuracy == 1){
-        ServiceReceived(); /* FIRE the ServiceReceived Event */
-        state = State.Inactive; /* Change the state to inactive to complete the final state. */
-
-        /* NOTE: This allows both the buyer and the seller to block the refund - the withdraw pattern should be used. */
-
-        buyer.transfer(value);
-        seller.transfer(this.balance);
+        if (slAccuracy >= sla){
+            ServiceReceived(); /* FIRE the ServiceReceived Event */
+            state = State.Inactive; /* Change the state to inactive to complete the final state. */
+            /* NOTE: This allows both the buyer and the seller to block the refund - the withdraw pattern should be used. */
+            buyer.transfer(value);
+            seller.transfer(this.balance);
         }
         else{revert();}
     }
-    /* */
-    function ripContract() public onlySeller {
+    /* Just playing around with a way to destroy the contracty to that it is not usable anymore. */
+    function ripContract() public onlySeller isLiar {
          selfdestruct(1);
-    
     }
 }
--- service_fact.js
+++ service_fact.js
@@ -6,52 +6,140 @@ var BIGML_USERNAME='donhagell';
 var BIGML_API_KEY='8224e14ebb93f8f81268b9593b57d8eb920575d0';
 var BIGML_AUTH="username="+BIGML_USERNAME+";api_key="+BIGML_API_KEY;
 var Dsource_resID = '';
+var headers = { 'content-type': 'application/json' };
 
-var headers = {
-    'content-type': 'application/json'
-};
-var url = 'https://bigml.io/source?' + BIGML_AUTH ;
  
-var dataString = '{"remote": "https://static.bigml.com/csv/iris.csv"}';
 
-var options = {
-    url: url,
+/************************************************************  
+*   Point datasource from remote file RF
+*   returns datasourceID RF
+*************************************************************/
+var dataString = '{"remote": "https://static.bigml.com/csv/iris.csv"}';
+var optionsRF = {
+    url: 'https://bigml.io/source?' + BIGML_AUTH ,
     method: 'POST',
     headers: headers,
     body: dataString
-};
-console.log(options);
-
-function callback(error, response, body) {
+}; // console.log(options);
+function callbackRF(error, response, body) {
     if (!error && response.statusCode == 200) {
         console.log(body);
+        console.log(body['resource']);
+		
     }
 	console.log(response);
 }
+//request(optionsRF, callbackRF);
 
-request(options, callback);
 
-// Upload datasource
-var foptions = {
-    url: url,
+/************************************************************  
+*   Upload datasource from @file local FI
+*   returns datasourceID FI
+*************************************************************/
+var optionsFI = {
+    url: 'https://bigml.io/source?' + BIGML_AUTH ,
+    headers: headers,
 	file: '@iris.csv',
 	'limit-rate': '500k',
 	encoding: null
-};
-
-function fcallback(error, response, body) {
+}; // console.log(options);
+function callbackFI(error, response, body) {
     if (!error && response.statusCode == 200) {
-
-		//console.log("\n\nexc:" + JSON.stringify(response) );
-
-        console.log(response.headers['content-type'])
-        console.log(response.headers['data'])
+		console.log("\n\nexc:" + JSON.stringify(response) );
+        //console.log(response.headers['content-type'])
+        //console.log(response.headers['data'])
     }
 	console.log("done");
 }
+//request(optionsFI, callbackFI) ;
 
-request(foptions, fcallback) ;
 
-// Create dataset
+/************************************************************  
+*   Create a dataset from the datasourceID FI. 
+*   returns datasetID DS
+*************************************************************/
+var optionsDS = {
+    url: 'https://bigml.io/dataset?' + BIGML_AUTH ,
+    headers: headers,
+	'source': 'source/4f52824203ce893c0a000053',
+	encoding: null
+}; // console.log(options);
+function callbackDS(error, response, body) {
+    if (!error && response.statusCode == 200) {
+		console.log("\n\ResponseSTR:" + JSON.stringify(response) );
+    }
+	console.log("done");
+}
+// request(optionsDS, callbackDS) ;
 
+
+/************************************************************  
+*   Create a model from the datasetID DS
+*   returns modelID MOD
+*************************************************************/
+var optionsMOD = {
+    url: 'https://bigml.io/model?' + BIGML_AUTH ,
+    headers: headers,
+	'dataset': 'dataset/4f52da4303ce896fe3000000',
+	encoding: null
+}; // console.log(options);
+function callbackMOD(error, response, body) {
+    if (!error && response.statusCode == 200) {
+		console.log("\n\ResponseSTR:" + JSON.stringify(response) );
+    }
+	console.log("done");
+}
+//request(optionsMOD, callbackMOD) ;
+
+
+/************************************************************  
+*   Create a prediction using the modelID MOD
+*   returns predictionID PRED
+*************************************************************/
+var optionsMOD = {
+    url: 'https://bigml.io/prediction?' + BIGML_AUTH ,
+    headers: headers,
+	'model': 'model/4f52e5ad03ce898798000000', 
+	'input_data': {'000000': 5, '000001': 3, '000002': 4}
+}; // console.log(options);
+function callbackMOD(error, response, body) {
+    if (!error && response.statusCode == 200) {
+		console.log("\n\ResponseSTR:" + JSON.stringify(response) );
+    }
+	console.log("done");
+}
+//request(optionsMOD, callbackMOD) ;
+
+/************************************************************  
+*   Retrieve a prediction using the predictionID PRED
+*   returns predictionID PRED
+*************************************************************/
+var optionsPRED = {
+    url: 'https://bigml.io/prediction?' + BIGML_AUTH ,
+    headers: headers,
+	'resource': 'prediction/5a31d44f59f5c342260003fd'
+}; // console.log(options);
+function callbackPRED(error, response, body) {
+    if (!error && response.statusCode == 200) {
+		//console.log("\n\ResponseSTR:" + JSON.stringify(response) );
+		var resp = JSON.parse(response.body)
+		//console.log("\n\ResponseJSON:" + resp.meta );
+		//console.log("\n\ResponseJSON:" + JSON.stringify(resp) );
+		//console.log("\n\ResponseJSON:" + resp.objects );
+		console.log("\n\ResponseJSON:" + JSON.stringify(  resp.objects[0].fields ) );
+		console.log("\n\ResponseJSON:" + JSON.stringify(  resp.objects[0].importance ) );
+		console.log("\n\ResponseJSON:" + JSON.stringify(  resp.objects[0].probabilities ) );
+		console.log("\n\ResponseJSON:" + JSON.stringify(  resp.objects[0].prediction ) );
+		console.log("\n\ResponseJSON:" + JSON.stringify(  resp.objects[0].name ) );
+		/*
+		for (x in resp.objects) {
+			console.log("\n\ResponseJSON:" + JSON.stringify( resp.objects[x].fields ) );
+			console.log("\n\ResponseJSON:" + JSON.stringify( resp.objects[x].importance ) );
+			probabilities
+		}*/
+		
+    }
+	console.log("done");
+}
+request(optionsPRED, callbackPRED) ;
 

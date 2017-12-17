2bd8b92b463c2fa0f973f704b22d136937b22167
Redhio Don <don@redh.io>
2017-12-16 22:04:31 -0500

    Saturday V0.10.04

--- contracts/RemoteServicePurchase.sol
+++ contracts/RemoteServicePurchase.sol
@@ -1,34 +1,46 @@
 pragma solidity ^0.4.11;
 /* ced-capstone - This contract governs the transaction for a remote machine learning service.  
 * The three entities which interact with the contract are the buyer, seller, and the external remote services.
-*
-* Friday V0.0.3
+*  For a Utility model, we would like this to be like a Pre-Authorized-Debit (PAD)
+* Saturday V0.0.4
 */
 contract Purchase {
     uint public value;
+    /* These are roles for this contract.  This is a four (4) entity service agreement with a potential for 
+    *  whitelisted operators.  By default he WL is to incude at least the buyer address */
     address public seller;
     address public buyer;
+    address public operator;
     address public service;
-    int public sla;
-    int public slAccuracy;
+    /* These are the contract terms and agreement */
+    uint public modelID;  // customer indicates which ML model to run using an ID
+    uint public slAccuracy;
+    uint public contractSLA;  // stores the min % accuracy the model must produce for contract to be valid, would be sent based on customer account, here we just enter it via web3 app
+    uint public contractRate;  // stores the contract rate ($charged to the cust) in wei, would be sent based on customer account, here we just enter it via web3 app
     
     enum State { Created, Locked, Inactive }
     State public state;
     /*
     * MLaaS - These should be in an abstracted class specific to the machine learning model 
     */
-    uint public modelID;
     uint public petalLength;
     uint public petalWidth;
     uint public sepalLength;
     string public prediction;
     
 
-    /* Set the purchase price requirement of the remote machine learning service. */
-    function Purchase(uint _modelID, uint _petalLength, uint _petalWidth, uint _sepalLength) public payable {
+    /* Set the purchase price and model requirement of the remote machine learning service. Purchase price is hardcoded at 2ether*/
+    function Purchase(uint _modelID, uint _contractRate) public payable {
+        require( contractRate  == msg.value); /* Check the funds before we burn any additional gas */
         seller = msg.sender;
-        require(  2  == msg.value);
+        value = msg.value;
         modelID = _modelID;
+        contractRate = _contractRate ;
+        
+    }
+    /* Set the purchase price requirement of the remote machine learning service. */
+    function Predict( uint _petalLength, uint _petalWidth, uint _sepalLength) public payable onlyBuyer {
+        require( contractRate  == msg.value); /* Check the funds before we burn any additional gas */
         petalLength = _petalLength;
         petalWidth = _petalWidth;
         sepalLength = _sepalLength;        
@@ -48,6 +60,11 @@ contract Purchase {
         require(msg.sender == seller);
         _;
     }
+    /* Check to see the message was sent by the Seller who is the owner, same as onlySeller() */
+    modifier isOwner() {
+        require(msg.sender == seller);
+        _;
+    }
     /* Check to see the message was sent by the RemoteService */
     modifier onlyService() {
         require(msg.sender == service);
@@ -55,12 +72,12 @@ contract Purchase {
     }
     /* Check to see the 'Service Level Acheived is greater or equal to Service Level Agreed' for the machine learning RemoteService */
     modifier isSLA() {
-        require(slAccuracy >= sla);
+        require(slAccuracy >= contractSLA);
         _;
     }
     /* Check to see the seller is not a liar */
     modifier isLiar() {
-        require(slAccuracy < sla);
+        require(slAccuracy < contractSLA);
         _;
     }
     /* Restrict the function to only a certain State.  Useful to step through a sequence of events*/
@@ -70,8 +87,8 @@ contract Purchase {
     }
     /* Define all of the contract events here.  One for each state  */
     event Aborted();
-    event PurchaseConfirmed();
-    event ServiceReceived();
+    event PurchaseConfirmed(address buyer, address seller, uint amount, uint256 sla);
+    event PredictionReceived();
 
     /* Abort the purchase and reclaim the ether by the buyer or seller before
     *  the contract is locked for servicing. */
@@ -81,9 +98,9 @@ contract Purchase {
         seller.transfer(this.balance);
     }
 
-    /* Confirm the purchase and the price to further lock the ether until the service is received. confirmReceived()*/
+    /* Confirm the purchase contract and the price to further lock the ether until the service is received. confirmReceived()*/
     function confirmPurchase() public inState(State.Created) isEqual(msg.value == (2 * value)) payable {
-        PurchaseConfirmed(); /* FIRE the PurchaseConfirmed Event */
+        PurchaseConfirmed(buyer,seller,value,contractSLA); /* FIRE the PurchaseConfirmed Event */
         buyer = msg.sender;
         state = State.Locked;
     }
@@ -91,8 +108,8 @@ contract Purchase {
     /* Confirm that the buyer has received the prediction, sla and can unlocked the Ether. */
     function confirmReceived() public onlyBuyer isSLA inState(State.Locked)
     {
-        if (slAccuracy >= sla){
-            ServiceReceived(); /* FIRE the ServiceReceived Event */
+        if (slAccuracy >= contractSLA){
+            PredictionReceived(); /* FIRE the PredictionReceived Event */
             state = State.Inactive; /* Change the state to inactive to complete the final state. */
             /* NOTE: This allows both the buyer and the seller to block the refund - the withdraw pattern should be used. */
             buyer.transfer(value);
@@ -100,8 +117,10 @@ contract Purchase {
         }
         else{revert();}
     }
-    /* Just playing around with a way to destroy the contracty to that it is not usable anymore. */
+    
+    /* Just playing around with a way to destroy the contract to that it is not usable anymore. 
+    *  Uses teh sellers address as th owner */
     function ripContract() public onlySeller isLiar {
-         selfdestruct(1);
+         selfdestruct(seller);
     }
 }
